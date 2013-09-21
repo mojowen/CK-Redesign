85f5438627461fdf68088fed5998521fa44f6102
Scott Duncombe <srduncombe@gmail.com>
2013-09-21 13:25:24 -0700

    new db settings

--- wp-config.php
+++ wp-config.php
@@ -16,13 +16,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'kinderland');
+define('DB_NAME', 'ck');
 
 /** MySQL database username */
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'stinging');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
